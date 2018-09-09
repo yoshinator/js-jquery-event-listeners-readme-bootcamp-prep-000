931600401e9629223fc00d86a5cd2b136a67ed63
yoan ante <yoanante@gmail.com>
2018-09-09 22:13:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt(){
+      $('p').on('click', function(){
+      alert("Hey!");
+    });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if (e.which === 71){
+      alert("G pressed");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
