75c53a9dbd359f32ca205dfa272bb98ffc09b092
yoan ante <yoanante@gmail.com>
2018-09-09 22:11:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,8 @@ function frameIt() {
 
 function pressIt(){
   $('input').on('keydown', function(e){
-    if (e.key === 'g'){
-      alert("g pressed");
+    if (e.key === 'G'){
+      alert("G pressed");
     }
   });
 }
